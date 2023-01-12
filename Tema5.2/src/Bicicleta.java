6f1a083b8683128770f4661db2dd2698846d22d5
Sara <106694716+Sariiitaa9789@users.noreply.github.com>
2023-01-12 16:33:37 +0100

    Prueba Practica33

---
+++ Tema5.2/src/Bicicleta.java
@@ -0,0 +1,51 @@
+
+
+public class Bicicleta {
+
+	protected int VelocidadActual;
+	protected int PlatoActual;
+	protected int PiñonActual;
+	
+
+	
+	
+	public void acelerar() {
+		VelocidadActual *=2; 	
+		
+	}
+	
+	public void frenar() {
+		
+		VelocidadActual/=2;
+		
+	}
+	
+	public void CambiarPlato(int plato) {
+		
+		PlatoActual=plato;
+		
+	}
+	
+	public void cambiarPiñon(int piñon) {
+		
+		PiñonActual=piñon;
+		
+	}
+
+	public Bicicleta(int velocidadActual, int platoActual, int piñonActual) {
+		super();
+		VelocidadActual = velocidadActual;
+		PlatoActual = platoActual;
+		PiñonActual = piñonActual;
+	}
+
+	@Override
+	
+	public String toString() {
+		return "Bicicleta [VelocidadActual=" + VelocidadActual + ", PlatoActual=" + PlatoActual + ", PiñonActual="
+				+ PiñonActual + "]";
+	}
+	
+	
+	
+}
---
+++ Tema5.2/src/BicicletaPrueba.java
@@ -0,0 +1,27 @@
+
+
+public class BicicletaPrueba {
+
+	public static void main(String[] args) {
+	
+		Bicicleta mibicicleta, tubicicleta, bicicleta3;
+		
+		mibicicleta = new Bicicleta(20, 3, 4);
+		
+		System.out.println(mibicicleta);
+		
+		tubicicleta = new Bicicleta(40, 5, 6);
+		
+		System.out.println(tubicicleta);
+		
+		bicicleta3 = new BicicletaMontaña(25,2,2, 4);
+		
+		System.out.println(bicicleta3);
+		
+		
+
+	}
+	
+	
+
+}
